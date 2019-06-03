29ee2bb55e68a6c0b1b4b491655abd623ee4cac4
mavpanos <mavpanos@gmail.com>
2019-06-03 17:06:53 +0200

    sets global variables to fix NOTES of devtools::check()

---
+++ R/globals.R
@@ -0,0 +1,2 @@
+if(getRversion() >= "2.15.1") utils::globalVariables(c("bin", "freq_orig", "cf", "actual",
+                                                       "z", "bunch_region", "key", "value", "cf_graph"))
